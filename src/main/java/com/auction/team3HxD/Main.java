e25fa4ebd183e3aa9195c9fded16c48cb76c02bb
vhoang0502 <viethoang050207@gmail.com>
2026-04-14 16:50:18 +0700

    chore: update Main and module-info for JavaFX

--- src/main/java/com/auction/team3HxD/Main.java
+++ src/main/java/com/auction/team3HxD/Main.java
@@ -1,21 +1,18 @@
 package com.auction.team3HxD;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Ngon luon!");
-        Scene scene = new Scene(label, 400, 200);
-        stage.setScene(scene);
-        stage.setTitle("Team 3HxD - Auction System");
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Hệ thống đấu giá trực tuyến");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
+    public static void main(String[] args) { launch(args); }
 }
