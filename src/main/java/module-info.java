159df65ed13cc7b9ec4c2745181124e5f0da7ef0
ducnm <ducnm.cs@gmail.com>
2026-04-16 18:56:46 +0700

    feat: add requirements and dependencies for MySQL

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,10 @@ module com.auction.team3HxD {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.sql;
+    requires mysql.connector.j;
+
     opens com.auction.team3HxD to javafx.fxml;
+    opens com.auction.team3HxD.model to java.sql;
     exports com.auction.team3HxD;
 }
