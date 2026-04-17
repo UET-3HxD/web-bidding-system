a5a92e47107fd68a9de766a810cc8f9d2fd03e74
ducnm <ducnm.cs@gmail.com>
2026-04-17 22:54:28 +0700

    feat: complete login/register logic and connection to DB

---
+++ src/main/java/com/auction/team3HxD/util/Protocol.java
@@ -0,0 +1,10 @@
+package com.auction.team3HxD.util;
+
+public class Protocol {
+    public static final String LOGIN_SUCCESS = "L00";
+    public static final String LOGIN_ERR_WRONG = "L01";
+    public static final String LOGIN_ERR_ALREADY = "L02";
+
+    public static final String REG_SUCCESS = "R00";
+    public static final String REG_ERR_EXISTS = "R01";
+}
