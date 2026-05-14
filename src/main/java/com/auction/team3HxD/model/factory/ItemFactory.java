fc77d153a9c7072fb9f1c35b58c8feea91f6a54b
ducnm-cs <ducnm.cs@gmail.com>
2026-05-14 23:51:36 +0700

    Delete src/main/java/com/auction/team3HxD/model/factory directory

--- src/main/java/com/auction/team3HxD/model/factory/ItemFactory.java
+++
@@ -1,7 +0,0 @@
-package com.auction.team3HxD.model.factory;
-
-import com.auction.team3HxD.model.Item;
-
-public interface ItemFactory {
-    Item createItem(String name , String description , double startingPrice);
-}
