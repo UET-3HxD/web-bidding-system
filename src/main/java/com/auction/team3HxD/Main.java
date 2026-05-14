ac4e8f006b48d019ed93e4418cba38926d9ca52a
ducnm-cs <ducnm.cs@gmail.com>
2026-05-14 23:37:08 +0700

    Update Main.java

--- src/main/java/com/auction/team3HxD/Main.java
+++ src/main/java/com/auction/team3HxD/Main.java
@@ -1,7 +1,5 @@
 package com.auction.team3HxD;
 
-import com.auction.team3HxD.util.AppConfig;
-import com.auction.team3HxD.util.SocketService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,20 +8,6 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Kết nối socket một lần duy nhất cho cả vòng đời ứng dụng (nếu không dùng mock)
-        if (!AppConfig.isMockEnabled()) {
-            try {
-                SocketService.getInstance().connect(
-                        AppConfig.getServerHost(),
-                        AppConfig.getServerPort()
-                );
-                System.out.println("SocketService đã kết nối đến server.");
-            } catch (Exception e) {
-                e.printStackTrace();
-                // Có thể hiển thị cảnh báo cho người dùng ở đây
-            }
-        }
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setTitle("Hệ thống đấu giá trực tuyến");
@@ -31,15 +15,7 @@ public class Main extends Application {
         primaryStage.setMinWidth(600);
         primaryStage.setMinHeight(400);
         primaryStage.show();
-
-        // Khi đóng cửa sổ chính, giải phóng socket
-        primaryStage.setOnCloseRequest(event -> {
-            if (!AppConfig.isMockEnabled()) {
-                SocketService.getInstance().shutdown();
-                System.out.println("SocketService đã ngắt kết nối.");
-            }
-        });
-    }
+    }   
 
     public static void main(String[] args) {
         launch();
