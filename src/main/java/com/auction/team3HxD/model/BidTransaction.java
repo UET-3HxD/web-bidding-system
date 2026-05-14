df23747da1367100b31f75081bf654775eee5a6f
ducnm-cs <ducnm.cs@gmail.com>
2026-05-14 23:30:26 +0700

    Update BidTransaction.java

--- src/main/java/com/auction/team3HxD/model/BidTransaction.java
+++ src/main/java/com/auction/team3HxD/model/BidTransaction.java
@@ -2,41 +2,23 @@ package com.auction.team3HxD.model;
 
 import java.time.LocalDateTime;
 
-public class BidTransaction extends Entity {
-
+public class BidTransaction extends Entity{
     private int id;
-    private Auction auction;
-    private Bidder bidder;
+    private User bidder;
     private double amount;
     private LocalDateTime timestamp;
 
-    // ===== CONSTRUCTOR =====
-    public BidTransaction(Auction auction, Bidder bidder, double amount) {
-        this.auction = auction;
+    // Getter and setter
+    public User getBidder() {return bidder;}
+
+    public double getAmount() {return amount;}
+
+    public LocalDateTime getTimestamp() {return timestamp;}
+
+    // Constructor
+    public BidTransaction(User bidder , double amount) {
         this.bidder = bidder;
         this.amount = amount;
         this.timestamp = LocalDateTime.now();
     }
-
-    // ===== GETTER =====
-    public int getId() { return id; }
-
-    public Auction getAuction() { return auction; }
-
-    public Bidder getBidder() { return bidder; }
-
-    public double getAmount() { return amount; }
-
-    public LocalDateTime getTimestamp() { return timestamp; }
-
-    // ===== SETTER =====
-    public void setId(int id) { this.id = id; }
-
-    public void setAuction(Auction auction) { this.auction = auction; }
-
-    public void setBidder(Bidder bidder) { this.bidder = bidder; }
-
-    public void setAmount(double amount) { this.amount = amount; }
-
-    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
 }
