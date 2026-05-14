3efe7edcc92399e01e51693ace435cd2ddfb2550
ducnm-cs <ducnm.cs@gmail.com>
2026-05-14 23:33:22 +0700

    Update Vehicle.java

--- src/main/java/com/auction/team3HxD/model/Vehicle.java
+++ src/main/java/com/auction/team3HxD/model/Vehicle.java
@@ -1,25 +1,19 @@
 package com.auction.team3HxD.model;
 
+import java.time.LocalDateTime;
+
 public class Vehicle extends Item{
-    private String licensePlate;
-    private String model;
-    private int year;
-
-    //Constructor
-    public Vehicle(String name , String description , double startingPrice) {
-        super(name, description, startingPrice);
+    public Vehicle(int sellerId, String name, String desc, double price, String path) {
+        super(sellerId, name, desc, price, path);
     }
-
-    // Getter and setter
-    public void setLicensePlate(String licensePlate) {this.licensePlate = licensePlate;}
-
-    public String getLicensePlate() {return licensePlate;}
-
-    public void setModel(String model) {this.model = model;}
-
-    public String getModel() {return model;}
-
-    public void setYear(int year) {this.year = year;}
-
-    public int getYear() {return year;}
+    public Vehicle(int id, int sellerId, String name, String desc, double price,
+                      String path, String status, LocalDateTime createdAt) {
+        super(id, sellerId, name, desc, price, path, status, createdAt);
+    }
+    public String getItemType(){
+        return "VEHICLE";
+    }
+    public Vehicle(){
+        super();
+    }
 }
