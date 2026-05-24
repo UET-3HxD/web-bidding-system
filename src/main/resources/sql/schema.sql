687977d66575504306742c2e2acc2b3554e3ec17
ducnm <ducnm.cs@gmail.com>
2026-05-24 20:25:36 +0700

    fix: update universal SQL structure

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -2,12 +2,12 @@
 --
 -- Host: localhost    Database: auction_db
 -- ------------------------------------------------------
--- Server version	8.0.45
+-- Server version	8.0.46
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,65 +16,88 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `users`
+-- Table structure for table `auction_sessions`
 --
-USE auction_db;
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-                         `id` int NOT NULL AUTO_INCREMENT,
-                         `username` varchar(100) NOT NULL UNIQUE,
-                         `password` varchar(255) NOT NULL,
-                         `email` varchar(100) DEFAULT NULL,
-                         `role` varchar(50) NOT NULL,
-    -- Cột mới đây Captain: Tự động lấy thời gian hiện tại khi thêm user
-                         `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `items`;
-CREATE TABLE `items` (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
-                         seller_id INT NOT NULL,
-                         product_name VARCHAR(255) NOT NULL,
-                         description TEXT,
-                         starting_price DECIMAL(15, 2),
-                         image_path TEXT,
-    -- Cột phân loại quan trọng
-                         item_type ENUM('ELECTRONIC', 'ART', 'VEHICLE') NOT NULL,
-                         status ENUM('WAITING', 'LIVE', 'SOLD', 'APPROVED', 'REJECTED') DEFAULT 'WAITING',
-                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                         FOREIGN KEY (seller_id) REFERENCES users(id) ON DELETE CASCADE
-);
-USE auction_db;
 DROP TABLE IF EXISTS `auction_sessions`;
-CREATE TABLE auction_sessions (
-                                  id INT AUTO_INCREMENT PRIMARY KEY,
-                                  item_id INT NOT NULL,
-                                  start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                  end_time TIMESTAMP NOT NULL,
-                                  status ENUM('ACTIVE', 'FINISHED') DEFAULT 'ACTIVE',
-                                  FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
-);
-DROP TABLE IF EXISTS `bids`;
-CREATE TABLE bids (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      auction_id INT NOT NULL,
-                      user_id INT NOT NULL,
-                      bid_amount DECIMAL(15, 2) NOT NULL,
-                      bid_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                      FOREIGN KEY (auction_id) REFERENCES auction_sessions(id) ON DELETE CASCADE,
-                      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auction_sessions` (
+                                    `id` int NOT NULL AUTO_INCREMENT,
+                                    `item_id` int NOT NULL,
+                                    `start_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                                    `end_time` timestamp NOT NULL,
+                                    `status` enum('ACTIVE','FINISHED') DEFAULT 'ACTIVE',
+                                    `current_price` decimal(15,2) DEFAULT '0.00',
+                                    `bid_increment` decimal(15,2) DEFAULT '0.00',
+                                    PRIMARY KEY (`id`),
+                                    KEY `item_id` (`item_id`),
+                                    CONSTRAINT `auction_sessions_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Table structure for table `bids`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `bids`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bids` (
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `auction_id` int NOT NULL,
+                        `user_id` int NOT NULL,
+                        `bid_amount` decimal(15,2) NOT NULL,
+                        `bid_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                        PRIMARY KEY (`id`),
+                        KEY `auction_id` (`auction_id`),
+                        KEY `user_id` (`user_id`),
+                        CONSTRAINT `bids_ibfk_1` FOREIGN KEY (`auction_id`) REFERENCES `auction_sessions` (`id`) ON DELETE CASCADE,
+                        CONSTRAINT `bids_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `seller_id` int NOT NULL,
+                         `product_name` varchar(255) NOT NULL,
+                         `description` text,
+                         `starting_price` decimal(15,2) DEFAULT NULL,
+                         `image_path` text,
+                         `item_type` enum('ELECTRONIC','ART','VEHICLE') NOT NULL,
+                         `status` enum('WAITING','APPROVED','LIVE','SOLD','REJECTED') DEFAULT 'WAITING',
+                         `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                         PRIMARY KEY (`id`),
+                         KEY `seller_id` (`seller_id`),
+                         CONSTRAINT `items_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `username` varchar(100) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `email` varchar(100) DEFAULT NULL,
+                         `role` enum('USER','ADMIN','BANNED') NOT NULL DEFAULT 'USER',
+                         `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                         PRIMARY KEY (`id`),
+                         UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -85,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-04-17 21:05:43
+-- Dump completed on 2026-05-24 20:24:07
